7738c9df31ac41e4608d6626630c57f5d5e18abb
abhayahlawat <ahlawatabhay21@gmail.com>
2025-09-03 15:40:38 +0530

    Prod build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import ViteSitemapPlugin from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,6 +12,11 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    ViteSitemapPlugin({
+      hostname: 'https://l1ar.vercel.app',
+      changefreq: 'weekly',
+      priority: 0.7,
+    }),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
